e4662ccd3f70509b883dcbb49f22f9d2434213c2
yifuhhh <huangyifu.ee@gmail.com>
2019-05-03 16:30:23 -0500

    Add Sine Wave test

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # CarbonPrj_Plot
 Carbon and 2D Material term project
+--05032019--
+
+-16:26-
+Add a testing plotting py script.
---
+++ SineWave_test.py
@@ -0,0 +1,15 @@
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
+
+t = np.arange(0.0, 2.0, 0.01)
+s = 1 + np.sin(2 * np.pi * t)
+
+fig, ax = plt.subplots()
+ax.plot(t, s)
+
+ax.set(xlabel='time (s)', ylabel='voltage (mV)', title='A sine wave')
+ax.grid()
+
+fig.savefig("test.png")
+plt.show()
