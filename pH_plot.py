10cbb4031fd0e2fb815662dd3d94c5831526fdcb
yifuhhh <huangyifu.ee@gmail.com>
2019-05-04 23:16:16 -0500

    Si_CNT_GNR pH sensor plots

--- pH_plot.py
+++ pH_plot.py
@@ -6,17 +6,28 @@ import os
 
 def main():
 
-    root_dir = "/home/yifu/PycharmProjects/CarbonPrj_Plot/RawData"
-    files = os.listdir(root_dir)
-    files_csv = list(filter(lambda x: x[-4:]=='.csv', files))
+    root_GNR = "/home/yifu/PycharmProjects/CarbonPrj_Plot/RawData/GNR_pH"
+    files = os.listdir(root_GNR)
+    files_GNR = list(filter(lambda x: x[-4:]=='.csv', files))
+    plot_GNR(files_GNR, root_GNR)
 
+    root_CNT = "/home/yifu/PycharmProjects/CarbonPrj_Plot/RawData/CNT_pH"
+    files = os.listdir(root_CNT)
+    files_CNT = list(filter(lambda x: x[-4:]=='.csv', files))
+    plot_CNT(files_CNT, root_CNT)
+
+    files_Si_pHVth = "/home/yifu/PycharmProjects/CarbonPrj_Plot/RawData/Si_pH/pH_Vth.csv"
+    files_Si_IdVg = "/home/yifu/PycharmProjects/CarbonPrj_Plot/RawData/Si_pH/IdVg.csv"
+    plot_Si_pHVth(files_Si_pHVth)
+    plot_Si_IdVg(files_Si_IdVg)
+
+def plot_GNR(files_GNR, root_GNR):
     plt.figure()
-    for file in files_csv:
+    for file in files_GNR:
         pH = int(file[-5])
         if file[-6] == '1':
             pH = pH + 10
-        print(pH)
-        data = pd.read_csv(root_dir + '/' + file, header=None)
+        data = pd.read_csv(root_GNR + '/' + file, header=None)
         data = np.array(data)
         Vg = data[:, 0]
         Ids = data[:, 1]
@@ -26,9 +37,57 @@ def main():
     plt.ylabel('Ids (A)')
     plt.title('Transfer characteristics')
     plt.legend()
+    plt.savefig('GNR_pH_transfer.PNG')
     plt.show()
 
+def plot_CNT(files_CNT, root_CNT):
+    plt.figure()
+    for file in files_CNT:
+        pH = int(file[-5])
+        if file[-6] == '1':
+            pH = pH + 10
+        data = pd.read_csv(root_CNT + '/' + file, header=None)
+        data = np.array(data)
+        Vg = data[:, 0]
+        Ids = data[:, 1]
+        plt.plot(Vg, Ids, label="pH = " + str(pH))
 
+    plt.xlabel('Vg (V)')
+    plt.ylabel('Ids (A)')
+    plt.title('Transfer characteristics')
+    plt.legend()
+    plt.savefig('CNT_pH_transfer.PNG')
+    plt.show()
+
+def plot_Si_pHVth(files_Si_pHVth):
+    data = pd.read_csv(files_Si_pHVth, header=None)
+    data = np.array(data)
+    pH = data[:, 0]
+    Vth = data[:, 1]
+    plt.plot(pH, Vth)
+    plt.xlabel('pH')
+    plt.ylabel('Vth (V)')
+    plt.title('pH - Vth relation')
+    plt.savefig('Si_pH_Vth.PNG')
+    plt.show()
+
+def plot_Si_IdVg(files_Si_IdVg):
+    data = pd.read_csv(files_Si_IdVg, header=None)
+    data = np.array(data)
+    plt.figure()
+    pH = 2
+    while pH < 13:
+        Vg = data[301*(pH-2):301*(pH-1)-1, 0]
+        Ids = data[301*(pH-2):301*(pH-1)-1, 1]
+        plt.plot(Vg, Ids, label="pH = " + str(pH))
+        pH = pH + 1
+
+    plt.xlabel('Vg (V)')
+    plt.ylabel('Ids (A)')
+    plt.title('Transfer characteristics')
+    plt.legend()
+    plt.savefig('Si_pH_transfer.PNG')
+    plt.show()
 
 if __name__ == "__main__":
     main()
